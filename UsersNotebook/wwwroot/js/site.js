e235a7a3292465e2c36a577ed609bcca1a324309
Marcin <Marcin.koz52@gmail.com>
2023-10-21 09:02:44 +0200

    Dodanie prostej obsługi błedów na stronie

--- UsersNotebook/wwwroot/js/site.js
+++ UsersNotebook/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function showToast(message) {
+    var toastHtml = `
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="5000">
+            <div class="toast-header">
+                <strong class="mr-auto">Błąd</strong>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="toast-body">${message}</div>
+        </div>`;
+    var toastElement = $(toastHtml);
+    $('body').append(toastElement);
+    toastElement.toast('show');
+}
 
-// Write your JavaScript code.
+$(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+    if (jqxhr.responseJSON && jqxhr.responseJSON.message) {
+        showToast(jqxhr.responseJSON.message);
+    } else {
+        showToast('Wystąpił nieznany błąd');
+    }
+});
