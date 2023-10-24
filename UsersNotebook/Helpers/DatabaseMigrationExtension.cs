75302a6023320eba9968119c8d0402229cbbf8dd
Marcin <Marcin.koz52@gmail.com>
2023-10-24 19:21:12 +0200

    Dodanie automatycznej migracji

---
+++ UsersNotebook/Helpers/DatabaseMigrationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersNotebook.Data.Context;
+
+namespace UsersNotebook.Helpers
+{
+    public static class DatabaseMigrationExtension
+    {
+        public static IApplicationBuilder MigrateDatabase(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.Migrate();
+            }
+
+            return app;
+        }
+    }
+}
